7c3b5f3c1242860a8629da130a5d211754ff1486
ume-kun1015 <baymax.blackandwhite@gmail.com>
2024-08-26 10:08:31 +0900

    fix: generate route for pagination

--- utils/config/nitro/getContentRoutes.ts
+++ utils/config/nitro/getContentRoutes.ts
@@ -1,13 +1,37 @@
-import { globSync } from 'glob'
+import { readdirSync } from 'fs'
 
 export const getContentRoutes = (): string[] => {
-  const routeNames = globSync('src/content/**/*.md').map((f) => {
-    const replaced = f
-      .replaceAll('\\', '/')
-      .replaceAll('src/content', '')
-      .replace('.md', '')
-    return `/posts${replaced}`
-  })
+  let contentRoutes: string[] = []
 
-  return [...routeNames]
+  readdirSync('./src/content', { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .forEach((dirent) => {
+      const dirRef = readdirSync(`./src/content/${dirent.name}`, { withFileTypes: true })
+
+      const postRoutes = dirRef.map((f) => {
+        const replaced = f.name
+          .replaceAll('\\', '/')
+          .replaceAll('src/content', '')
+          .replace('.md', '')
+
+        return `/posts/${dirent.name}/${replaced}`
+      })
+
+      const count = dirRef.filter((subDirent) => subDirent.isFile() && subDirent.name.includes('.md')).length
+
+      let maxPageNum = Math.floor(count / 5)
+      // ページネーションのため、あまりが出たときはページ数を 1 追加する
+      if (count % 5 > 0) {
+        maxPageNum++
+      }
+
+      const categoryRoutes = Array.from({ length: maxPageNum }, (_, pageNum) => pageNum + 1)
+        .map((pageNum) => {
+          return `/posts/categories/${dirent.name}/${pageNum}`
+        })
+
+      contentRoutes = [...contentRoutes, ...postRoutes, ...categoryRoutes]
+    })
+
+  return contentRoutes
 }
