ccc467e606a8211ffa41fb40924e4bd5e575ffe1
ume-kun1015 <baymax.blackandwhite@gmail.com>
2024-09-13 13:55:15 +0900

    feat: add sitemap generate event handler

---
+++ src/server/routes/sitemap.xml.ts
@@ -0,0 +1,27 @@
+import { join } from 'path'
+
+import { defineEventHandler } from '#imports'
+import { SitemapStream, streamToPromise } from 'sitemap'
+
+import { serverQueryContent } from '#content/server'
+
+export default defineEventHandler(async (event) => {
+  const docs = await serverQueryContent(event).find()
+  const sitemap = new SitemapStream({
+    hostname: 'https://offich.me',
+    lastmodDateOnly: true,
+  })
+
+  for (const doc of docs) {
+    sitemap.write({
+      url: join('/posts', doc._path),
+      changefreq: 'weekly',
+      lastmod: doc.publishedAt,
+      priority: doc.priority || 0.5,
+    })
+  }
+
+  sitemap.end()
+
+  return streamToPromise(sitemap)
+})
