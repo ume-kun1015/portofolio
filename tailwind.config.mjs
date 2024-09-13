c7595b2995ca0db282f5175b641dced27d815988
ume-kun1015 <baymax.blackandwhite@gmail.com>
2024-09-13 16:52:11 +0900

    feat: add spacing

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,28 @@ import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
   theme: {
+    spacing: {
+      0: '0px',
+      0.5: '4px',
+      1: '8px',
+      1.5: '12px',
+      2: '16px',
+      2.5: '20px',
+      3: '24px',
+      4: '32px',
+      5: '40px',
+      6: '48px',
+      7: '56px',
+      8: '64px',
+      9: '72px',
+      10: '80px',
+      11: '88px',
+      12: '96px',
+      13: '104px',
+      14: '112px',
+      15: '120px',
+    },
+
     screens: {
       pc: { min: '520px' },
     },
