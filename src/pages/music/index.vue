334f08be81f068db0d7cea4cc392c3e66b01444c
ume-kun1015 <baymax.blackandwhite@gmail.com>
2024-09-21 10:44:12 +0900

    feat: noindex in pending pages

--- src/pages/music/index.vue
+++ src/pages/music/index.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import { useHead, useSeoMeta } from '#imports'
+
+useHead({
+  title: 'Music',
+})
+
+useSeoMeta({
+  robots: 'noindex',
+})
+</script>
+
 <template>
   <div class="p-2 max-w-6xl mx-auto min-h-screen">
     <p>coming soon...</p>
